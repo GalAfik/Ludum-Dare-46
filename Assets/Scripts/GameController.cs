e9021c9204d82637bcdc50d8b9f25e8d783103ba
Gal Afik <gal.p.afik@gmail.com>
2020-04-17 23:47:51 -0400

    Added a game controller object to keep track of timer vars

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Ludum_Dare_46
+{
+	public class GameController : MonoBehaviour
+	{
+		[Serializable]
+		public class ConfigurationData
+		{
+			public float MaxGateTimer; // The timer for getting to the current gate
+			public float InitialPhoneCharge; // The charge on the phone at the start of a game
+			[Range(0, 5)] public float PhoneChargeDrainRate; // How fast the phone will drain
+		}
+		public ConfigurationData Conf = new ConfigurationData();
+
+		private float CurrentGateTimer; // The timer for getting to the current gate
+		private float CurrentPhoneCharge; // The current charge on the phone battery
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			// Set the initial timer values
+			CurrentGateTimer = Conf.MaxGateTimer;
+			CurrentPhoneCharge = Conf.InitialPhoneCharge;
+
+			StartCoroutine(ResetGateTimer());
+		}
+
+		IEnumerator ResetGateTimer()
+		{
+			while (CurrentGateTimer > 0)
+			{
+				// Wait for one second
+				yield return new WaitForSeconds(1);
+				// Decrement the gate timer
+				CurrentGateTimer--;
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			// Handle phone charge timer
+			if (CurrentPhoneCharge > 0) CurrentPhoneCharge -= Conf.PhoneChargeDrainRate * Time.deltaTime;
+			else CurrentPhoneCharge = 0; // TODO :: This is where the player should lose the game!
+
+			print("GameController::CurrentGateTimer = " + CurrentGateTimer);
+			print("GameController::CurrentPhoneCharge = " + CurrentPhoneCharge);
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,46 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class Player : MonoBehaviour
+namespace Ludum_Dare_46
 {
-	[Serializable]
-	public class ConfigurationData
+	public class Player : MonoBehaviour
 	{
-		public float MoveSpeed; // How fast a player moves through the world
-	}
-	public ConfigurationData Conf = new ConfigurationData();
-	private Vector2 MoveVector; // The current movement vector of the player
-	private float PreviousHorizontal = -1; // The last known horizontal movement of the player
+		[Serializable]
+		public class ConfigurationData
+		{
+			public float MoveSpeed; // How fast a player moves through the world
+		}
+		public ConfigurationData Conf = new ConfigurationData();
+		private Vector2 MoveVector; // The current movement vector of the player
+		private float PreviousHorizontal = -1; // The last known horizontal movement of the player
 
-	private Animator Animator; // Animation Controller
+		private Animator Animator; // Animation Controller
 
-	private void Awake()
-	{
-		// Get the Animator object
-		Animator = GetComponent<Animator>();
-	}
+		private void Awake()
+		{
+			// Get the Animator object
+			Animator = GetComponent<Animator>();
+		}
 
-	// Update is called once per frame
-	void Update()
-    {
-		// Reset the movement vector
-		MoveVector = Vector2.zero;
+		// Update is called once per frame
+		void Update()
+		{
+			// Reset the movement vector
+			MoveVector = Vector2.zero;
 
-		// Add the current axis inputs to the player's movement
-		MoveVector.x = Input.GetAxis("Horizontal");
-		MoveVector.y = Input.GetAxis("Vertical");
+			// Add the current axis inputs to the player's movement
+			MoveVector.x = Input.GetAxis("Horizontal");
+			MoveVector.y = Input.GetAxis("Vertical");
 
-		// Apply force to the player's movement
-		transform.position += (Vector3) MoveVector.normalized * Conf.MoveSpeed * Time.deltaTime;
+			// Apply force to the player's movement
+			transform.position += (Vector3)MoveVector.normalized * Conf.MoveSpeed * Time.deltaTime;
 
-		// Set Animator vars
-		Animator.SetFloat("MoveSpeed", MoveVector.magnitude);
+			// Set Animator vars
+			Animator.SetFloat("MoveSpeed", MoveVector.magnitude);
 
-		// Check if the player has switched horizontal movement directions
-		if (MoveVector.normalized.x * PreviousHorizontal < 0)
-			Animator.SetTrigger("FlipTrigger");
+			// Check if the player has switched horizontal movement directions
+			if (MoveVector.normalized.x * PreviousHorizontal < 0)
+				Animator.SetTrigger("FlipTrigger");
 
-		// Set the last known horizontal movement value
-		if (MoveVector.x != 0) PreviousHorizontal = MoveVector.normalized.x;
+			// Set the last known horizontal movement value
+			if (MoveVector.x != 0) PreviousHorizontal = MoveVector.normalized.x;
+		}
 	}
 }
